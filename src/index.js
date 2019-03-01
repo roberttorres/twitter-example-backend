65a00a587e8b6d2ee0285ea6db2f90919fbea10e
roberttorres <robertorres@gmail.com>
2019-03-01 01:44:20 -0300

    Configura porta do Heroku

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes"));
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port 3000");
 });
